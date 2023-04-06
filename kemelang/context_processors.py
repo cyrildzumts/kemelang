1856630df8f913ead144f3d4be03641068d3c54f
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-04-07 00:21:37 +0200

    wip

---
+++ kemelang/context_processors.py
@@ -0,0 +1,18 @@
+from kemelang import settings
+from django.contrib.auth.models import User
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def site_context(request):
+
+    context = {
+        'site_name' : settings.SITE_NAME,
+        'SITE_NAME' : settings.SITE_NAME,
+        'SITE_HOST' : settings.SITE_HOST,
+        'redirect_to' : '/',
+        'dev_mode' : settings.DEV_MODE,
+        'next_url' : request.path,
+    }
+    return context
