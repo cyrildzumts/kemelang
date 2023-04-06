dfbd405d6008800a41d5544c4d723ac97b28839e
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-04-06 23:19:50 +0200

    initial commit

--- kemelang/settings.py
+++ kemelang/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import ugettext_lazy as _
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
