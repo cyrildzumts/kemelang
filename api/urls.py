1850cb23e5c58547889b3d530e81cb430aefd681
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-04-09 14:06:18 +0200

    wip

--- api/urls.py
+++ api/urls.py
@@ -8,16 +8,16 @@ app_name = "api"
 api_patterns = [
     path('countries/', views.countries, name="countries"),
     path('langages/', views.langages, name="langages"),
-    path('country-langages/<slug:country_slug>/<uuid:country_uuid>/', views.langages, name="langages"),
     path('create-country/', views.create_country, name="country-create"),
     path('create-langage/', views.create_langage, name="langage-create"),
+    path('search-langage/', views.search_langage, name="langage-search"),
     path('create-word/', views.create_word, name="word-create"),
+    path('search-country/', views.search_country, name="country-search"),
+    path('detect-langage/', views.detect_langage, name="langage-detect"),
+    path('country-langages/<slug:country_slug>/<uuid:country_uuid>/', views.country_langages, name="country-langages"),
     path('update-word/<str:word>/<uuid:word_uuid>/', views.update_word, name="word-update"),
     path('update-country/<slug:country_slug>/<uuid:country_uuid>/', views.update_country, name="country-update"),
     path('update-langage/<slug:langage_slug>/<uuid:langage_uuid>/', views.update_langage, name="langage-update"),
-    path('search-langage/', views.search_langage, name="langage-search"),
-    path('search-country/', views.search_country, name="country-search"),
-    path('detect-langage/', views.detect_langage, name="langage-detect"),
     path('word-synonymes/<str:word>/<uuid:word_uuid>/', views.word_synonymes, name="word-synonymes"),
     
 ]
