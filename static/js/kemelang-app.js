6b41d0cb516a6e757061586c88863d43b629ca0a
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-04-13 19:10:48 +0200

    wip

--- static/js/kemelang-app.js
+++ static/js/kemelang-app.js
@@ -7,12 +7,14 @@ requirejs.config({
     waitSeconds: 0
 });
 
-requirejs(['accounts','scroll','ajax_api', 'core','country_manager', 'langage_manager', 'commons', 'image_loader', 'editor_api', 'image_loader'], function(account, scroll_tools,ajax_api, core, CountryManager, LangageManager ){
+requirejs(['accounts','scroll','ajax_api', 'core','country_manager', 'langage_manager','word_manager', 'commons', 'image_loader', 'editor_api', 'image_loader'], function(account, scroll_tools,ajax_api, core, CountryManager, LangageManager, WordManager ){
     account.init();
     scroll_tools.init();
     let countryManager = new CountryManager();
     countryManager.init();
     let langageManager = new LangageManager();
     langageManager.init();
+    let wordManager = new WordManager();
+    wordManager.init();
     console.log("JQuery version :", $().jquery);
 });
