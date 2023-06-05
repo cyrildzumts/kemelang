2b76931af2e002c66628fb9c0ed6ff6287c4a2a6
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-06-05 23:00:14 +0200

    wip

--- dictionary/models.py
+++ dictionary/models.py
@@ -121,7 +121,7 @@ class Word(models.Model):
     transliteration = models.CharField(max_length=constants.WORD_MAX_LENGTH, blank=True, null=True)
     audio = models.FileField(upload_to=upload_word_audio_to, blank=True, null=True)
     synonymes = models.ManyToManyField('self', blank=True)
-    #translations = models.ManyToManyField('self', blank=True)
+    translations = models.ManyToManyField('self', blank=True)
     langage = models.ForeignKey(Langage, on_delete=models.CASCADE, related_name='words')
     description = models.JSONField(blank=True, null=True)
     view_count = models.IntegerField(default=0)
