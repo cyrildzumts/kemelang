46f67d3f46e7e7d0c82ca401304560fd32d6c6d3
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-06-18 23:29:59 +0200

    wip

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,8 +7,8 @@ import uuid
 class Settings(models.Model):
     maintenance_mode = models.BooleanField(default=True, blank=True, null=True)
     allow_anonyme_user = models.BooleanField(default=True, blank=True, null=True)
-    added_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='added_words', null=True, blank=True)
-    changed_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='changed_words', blank=True, null=True)
+    added_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='added_settings', null=True, blank=True)
+    changed_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='changed_settings', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     changed_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     setting_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
