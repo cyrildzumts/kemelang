8df8645a31441a2303b852bd806a72d899094a1e
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-06-18 23:49:08 +0200

    wip

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,7 +7,7 @@ from dashboard import views
 app_name = "dashboard"
 dashboard_patterns = [
     path('', views.dashboard, name="dashboard"),
-    path('settings/', views.settings, name="settings"),
+    path('settings/', views.dashboard_settings, name="settings"),
     path('create-settings/', views.create_settings, name="settings-create"),
     path('update-settings/<uuid:setting_uuid>', views.update_settings, name="settings-update"),    
 ]
