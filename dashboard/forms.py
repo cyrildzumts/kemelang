b4eb6934c1216a15c0dace344d85d3158e932830
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-06-23 17:58:29 +0200

    wip

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django import forms
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from dashboard.models import Settings
@@ -8,4 +9,12 @@ class SettingsForm(forms.ModelForm):
     class Meta:
         model = Settings
         fields = Settings.FORM_FIELDS
+    
+    # def clean_maintenance_mode(self):
+    #     value = self.cleaned_data.get('maintenance_mode')
+    #     if value is None:
+    #         value = True
+    #     return value
 
+    def clean(self):
+        return super().clean()
