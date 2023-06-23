3c1e4a5ded97478a2f66d0f824e7d540019deeaa
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-06-23 19:08:31 +0200

    wip

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -6,15 +6,20 @@ from dashboard.models import Settings
 
 class SettingsForm(forms.ModelForm):
     
+    maintenance_mode = forms.BooleanField(required=True, initial=True)
+    
     class Meta:
         model = Settings
         fields = Settings.FORM_FIELDS
+        
     
-    # def clean_maintenance_mode(self):
-    #     value = self.cleaned_data.get('maintenance_mode')
-    #     if value is None:
-    #         value = True
-    #     return value
+        
+    
+    def clean_maintenance_mode(self):
+        value = self.cleaned_data.get('maintenance_mode')
+        if value is None:
+            value = True
+        return value
 
     def clean(self):
         return super().clean()
