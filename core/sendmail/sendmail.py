772285937ea6b4f22817c32dde6487f0278d0e5d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-07-05 00:10:21 +0200

    wip

--- core/sendmail/sendmail.py
+++ core/sendmail/sendmail.py
@@ -2,6 +2,7 @@ import smtplib
 from email.mime.text import MIMEText
 from typing import Iterable, Optional
 from django.core.mail.backends.smtp import EmailBackend
+from django.core.mail.utils import DNS_NAME
 from django.core.mail.message import EmailMessage
 from kemelang import settings
 import logging
@@ -23,13 +24,45 @@ class CoreEmailBackend(EmailBackend):
     
     def open(self):
         logger.info(f"Opening CoreEmailBackend using TLS - type of SSS Context {type(self.ssl_context)} - SSL-Context : {self.ssl_context}")
-        return super().open()
+        #return super().open()
+        """
+        Ensure an open connection to the email server. Return whether or not a
+        new connection was required (True or False) or None if an exception
+        passed silently.
+        """
+        if self.connection:
+            # Nothing to do if the connection is already open.
+            return False
+
+        # If local_hostname is not specified, socket.getfqdn() gets used.
+        # For performance, we use the cached FQDN for local_hostname.
+        connection_params = {'local_hostname': DNS_NAME.get_fqdn()}
+        if self.timeout is not None:
+            connection_params['timeout'] = self.timeout
+        if self.use_ssl:
+            connection_params.update({
+                'keyfile': self.ssl_keyfile,
+                'certfile': self.ssl_certfile,
+            })
+        try:
+            self.connection = self.connection_class(self.host, self.port, **connection_params)
+
+            # TLS/SSL are mutually exclusive, so only attempt TLS over
+            # non-secure connections.
+            if not self.use_ssl and self.use_tls:
+                self.connection.starttls(keyfile=self.ssl_keyfile, certfile=self.ssl_certfile)
+            if self.username and self.password:
+                self.connection.login(self.username, self.password)
+            return True
+        except OSError:
+            if not self.fail_silently:
+                raise
     
-    def send_messages(self, email_messages: Iterable[EmailMessage]) -> int:
-        if(self.use_tls):
-            logger.info(f"CoreEmailBackend using TLS - type of SSS Context {type(self.ssl_context)} -  SSL-Context : {self.ssl_context}")
-            self.connection.starttls()
-        return super().send_messages(email_messages)
+    # def send_messages(self, email_messages: Iterable[EmailMessage]) -> int:
+    #     if(self.use_tls):
+    #         logger.info(f"CoreEmailBackend using TLS - type of SSS Context {type(self.ssl_context)} -  SSL-Context : {self.ssl_context}")
+    #         self.connection.starttls()
+    #     return super().send_messages(email_messages)
     
     
 
