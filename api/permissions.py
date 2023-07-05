9e2e8e3029cb07ff81d7ff17b487c37677274b81
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-07-05 23:36:54 +0200

    wip

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,11 @@
 from rest_framework import permissions as REST_PERMS
 from core import permissions as PERMISSIONS
+import logging
 
+
+logger = logging.getLogger(__name__)
 class TranslatorPermission(REST_PERMS.BasePermission):
     def has_permission(self, request, view):
+        logger.info(f"Translator Permissions  Checker for user : {request.user}")
         return request.user.has_perm(PERMISSIONS.DASHBOARD_ACCESS_TRANSLATOR_PERM)
         
