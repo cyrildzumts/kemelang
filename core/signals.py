1ccc9f7e50985cd71d860b042c13e4d1df3edb30
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-07-05 10:02:26 +0200

    wip

--- core/signals.py
+++ core/signals.py
@@ -8,6 +8,7 @@ from accounts.models import Account
 from django.utils.text import slugify
 from django.utils import translation
 from core.tasks import send_mail_task
+from core.resources import ui_strings as CORE_UI_STRINGS
 from core import constants as CORE_CONSTANTS
 from django.core.cache.utils import make_template_fragment_key
 from kemelang import settings
@@ -25,15 +26,16 @@ def send_welcome_mail(sender, instance, created, **kwargs):
         if str(instance.username).startswith(settings.TEST_USER_PREFIX):
             logger.debug(f"sending welcome mail for test user {instance.username}...")
             return
+        mail_title = CORE_UI_STRINGS.UI_WELCOME_MAIL_TITLE % {'site_name': settings.SITE_NAME}
         email_context = {
             'use_template': True,
             'message': None,
             'template_name': settings.DJANGO_WELCOME_EMAIL_TEMPLATE,
-            'title': 'Bienvenu chez LYSHOP',
+            'title': mail_title,
             'recipient_email': instance.email,
             
             'context':{
-                'MAIL_TITLE': 'Bienvenu chez LYSHOP',
+                'MAIL_TITLE': mail_title,
                 'SITE_NAME': settings.SITE_NAME,
                 'SITE_HOST': settings.SITE_HOST,
                 'FULL_NAME': instance.get_full_name()
@@ -46,7 +48,7 @@ def send_welcome_mail(sender, instance, created, **kwargs):
         )
         admin_email_context = copy.deepcopy(email_context)
         admin_email_context['recipient_email'] = settings.ADMIN_EXTERNAL_EMAIL
-        admin_email_context['title'] = "Nouvel utilisateur"
+        admin_email_context['title'] = CORE_UI_STRINGS.UI_NEW_USER_MAIL_TITLE
         send_mail_task.apply_async(
             args=[admin_email_context],
             queue=settings.CELERY_OUTGOING_MAIL_QUEUE,
@@ -62,17 +64,18 @@ def send_validation_mail(sender, instance, created, **kwargs):
         if str(instance.user.username).startswith(settings.TEST_USER_PREFIX):
             logger.debug(f"sending validation mail for test user {instance.user.username} ...")
             return
+        mail_title = CORE_UI_STRINGS.UI_MAIL_VALIDATION_TITLE
         email_context = {
             'use_template': True,
             'message': None,
             'template_name': settings.DJANGO_VALIDATION_EMAIL_TEMPLATE,
-            'title': 'Validation de votre adresse mail',
+            'title': mail_title,
             'recipient_email': instance.user.email,
             'context':{
                 'SITE_NAME': settings.SITE_NAME,
                 'SITE_HOST': settings.SITE_HOST,
                 'FULL_NAME': instance.user.get_full_name(),
-                'MAIL_TITLE': "Validation d'E-Mail",
+                'MAIL_TITLE': mail_title,
                 'validation_url' : settings.SITE_HOST + instance.get_validation_url()
             }
         }
