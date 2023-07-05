4485b864550699b91f69823681c00548c8e596d1
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-07-05 12:49:51 +0200

    wip

--- core/sendmail/sendmail.py
+++ core/sendmail/sendmail.py
@@ -72,6 +72,7 @@ class EMailBackend(DefaultEmailBackend):
     
     @cached_property
     def ssl_context(self):
+        logger.info(f"ssl_context : using TLS  - SSL-Context : {self.ssl_context}")
         if self.ssl_certfile or self.ssl_keyfile:
             ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
             ssl_context.load_cert_chain(self.ssl_certfile, self.ssl_keyfile)
