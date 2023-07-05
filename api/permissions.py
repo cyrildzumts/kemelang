fc889a07abe408f41d29d3ed364cb88f0f11e09f
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2023-07-05 23:46:31 +0200

    wip

--- api/permissions.py
+++ api/permissions.py
@@ -6,6 +6,7 @@ import logging
 logger = logging.getLogger(__name__)
 class TranslatorPermission(REST_PERMS.BasePermission):
     def has_permission(self, request, view):
-        logger.info(f"Translator Permissions  Checker for user : {request.user}")
-        return request.user.has_perm(PERMISSIONS.DASHBOARD_ACCESS_TRANSLATOR_PERM)
+        has_perm = request.user.has_perm(PERMISSIONS.DASHBOARD_ACCESS_TRANSLATOR_PERM)
+        logger.info(f"Translator Permissions  Checker for user : {request.user} - HAS_PERM : {has_perm}")
+        return has_perm
         
